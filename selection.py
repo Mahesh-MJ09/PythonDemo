995c89d7a98a5722ec9505a5b28a01f1e206be5e
Mahesh <maheshjakkula3809@gmail.com>
2024-11-05 14:31:01 +0530

    Selection sorted...

---
+++ selection.py
@@ -0,0 +1,20 @@
+
+
+def sort(nums):
+
+    for i in range(5):
+        minpos = i
+        for j in range(i,6):
+            if nums[j] < nums[minpos]:
+                minpos = j
+
+
+        temp = nums[i]
+        nums[i] = nums[minpos]
+        nums[minpos] = temp
+        print(nums)
+
+
+nums = [2,38,16,99,1,5]
+sort(nums)
+print(nums)
