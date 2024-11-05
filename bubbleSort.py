2a3ed6e0491fe5dd4bb741cfec943b3e2d17f5e3
Mahesh <maheshjakkula3809@gmail.com>
2024-11-05 14:14:16 +0530

    sorted...

---
+++ bubbleSort.py
@@ -0,0 +1,15 @@
+
+
+def sort(nums):
+
+    for i in range(len(nums)-1 , 0 , -1):
+        for j in range(i) :
+            if nums[j] > nums[j+1]:
+                temp = nums[j]
+                nums[j] = nums[j+1]
+                nums[j+1] = temp
+
+
+nums = [38,16,54,99,1,2]
+sort(nums)
+print(nums)
