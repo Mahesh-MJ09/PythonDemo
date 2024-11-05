9409a5c8bebd5358d0db53683f4efb2a540be348
Mahesh <maheshjakkula3809@gmail.com>
2024-11-05 11:38:10 +0530

    taking input from user...

--- add.py
+++ add.py
@@ -1,14 +1,7 @@
 
 
+a = int(input("enter 1st number : "))
+b = int(input("enter 2nd number : "))
 
-a = 5
-b = 6
-
-c = a + b
+c = a + b     #sum of 2 nums
 print(f" the sum : " , c)
-#
-# a = int(input("enter 1st number : "))
-# b = int(input("enter 2nd number : "))
-#
-# c = a + b     #sum of 2 nums
-# print(f" the sum : " , c)
