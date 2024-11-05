45327977fe4c270b694873540ff600ffe0e44b72
Mahesh <maheshjakkula3809@gmail.com>
2024-11-05 11:35:55 +0530

    sum of 2 numbers...

---
+++ add.py
@@ -0,0 +1,14 @@
+
+
+
+a = 5
+b = 6
+
+c = a + b
+print(f" the sum : " , c)
+#
+# a = int(input("enter 1st number : "))
+# b = int(input("enter 2nd number : "))
+#
+# c = a + b     #sum of 2 nums
+# print(f" the sum : " , c)
